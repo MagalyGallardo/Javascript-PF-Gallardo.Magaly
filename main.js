39db15bf414135f3a3d241b9e03f9c102c90f82c
Magaly Gallardo <maggye.chayns@gmail.com>
2023-07-17 00:55:10 -0300

    se repara codigo javascript

--- main.js
+++ main.js
@@ -1,100 +1,102 @@
-            // Consultar precio de actividad (hay que escribir con las respectivas mayúsculas) 
-            // Se agregan Dom, se eliminan los prompt y alerts, se realiza evento
+  // Consultar precio de actividad (hay que escribir con las respectivas mayúsculas) 
+     // Se agregan Dom, se eliminan los prompt y alerts, se realiza evento
             
-            let actividadBuscada = document.getElementById("busquedaActividad");
+let actividadBuscada = document.getElementById("busquedaActividad");
 
-            function busquedaActividad() {
-              const precioActividad = [
-                { nombre: "Pase Libre", precio: 5000 },
-                { nombre: "CrossFit", precio: 3000 },
-                { nombre: "GAP", precio: 2500 },
-                { nombre: "Zumba", precio: 4200 },
-                { nombre: "Step", precio: 3500 },
-                { nombre: "Musculación", precio: 4000 },
-                { nombre: "Spinning", precio: 4100 },
-              ];
+function busquedaActividad() {
+  const precioActividad = [
+     { nombre: "Pase Libre", precio: 5000 },
+     { nombre: "CrossFit", precio: 3000 },
+     { nombre: "GAP", precio: 2500 },
+     { nombre: "Zumba", precio: 4200 },
+     { nombre: "Step", precio: 3500 },
+     { nombre: "Musculación", precio: 4000 },
+     { nombre: "Spinning", precio: 4100 },
+   ];
+  return precioActividad.find((el) => el.nombre === actividadBuscada.value);
+ }
+
+let buscar = document.getElementById("buscar");
+buscar.addEventListener("click", respuestaClick);
+
             
-              return precioActividad.find((el) => el.nombre === actividadBuscada.value);
-            }
-
-            let buscar = document.getElementById("buscar");
-            buscar.addEventListener("click", respuestaClick);
-
-            
-            function respuestaClick() {
-              let mensaje = document.getElementById("resultadoActividadBuscada");
-              let actividadEncontrada = busquedaActividad();
+function respuestaClick() {
+  let mensaje = document.getElementById("resultadoActividadBuscada");
+  let actividadEncontrada = busquedaActividad();
               
-              if (actividadEncontrada) {
-                mensaje.innerHTML = ("El precio de la actividad es de: $" + actividadEncontrada.precio);
-              } else {
-                mensaje.innerHTML = "Actividad no encontrada";
-              }
-            }
+  if (actividadEncontrada) {
+     mensaje.innerHTML = ("El precio de la actividad es de: $" + actividadEncontrada.precio);
+    } else {
+     mensaje.innerHTML = "Actividad no encontrada";
+    }
+  }
             
             
             //Proyecto Presupuesto
          
            
-          let actividadPresupuesto = document.querySelector("#actividadPresupuesto");
-          const radioSeleccionado = document.querySelector('input[type="radio"]:checked');
-          function presupuestoForm() {
+            let actividadPresupuesto = document.querySelector("#actividadPresupuesto");
+            const radioSeleccionado = document.querySelector('input[type="radio"]:checked');
+            
+            function presupuestoForm(e) {
               e.preventDefault();
-            let presupuesto = 0;
-          
-            if (radioSeleccionado.value === "si" && radioSeleccionado.name === "masculino") {
-              presupuesto = (actividadPresupuesto.value / 2) - (actividadPresupuesto.value * 10 / 100);
-            } else if (radioSeleccionado.value === "si" && radioSeleccionado.name === "femenino") {
-              presupuesto = (actividadPresupuesto.value / 2) - (actividadPresupuesto.value * 5 / 100);
-            } else if (radioSeleccionado.value === "no" && radioSeleccionado.name === "masculino") {
-              presupuesto = (actividadPresupuesto.value * 10) / 100;
-            } else {
-              presupuesto = (actividadPresupuesto.value * 5) / 100;
+              let presupuesto = 0;
+            
+              if (radioSeleccionado.value === "si" && radioSeleccionado.name === "masculino") {
+                presupuesto = (actividadPresupuesto.value / 2) - (actividadPresupuesto.value * 10 / 100);
+              } else if (radioSeleccionado.value === "si" && radioSeleccionado.name === "femenino") {
+                presupuesto = (actividadPresupuesto.value / 2) - (actividadPresupuesto.value * 5 / 100);
+              } else if (radioSeleccionado.value === "no" && radioSeleccionado.name === "masculino") {
+                presupuesto = (actividadPresupuesto.value * 10) / 100;
+              } else {
+                presupuesto = (actividadPresupuesto.value * 5) / 100;
+              }
+            
+              return presupuesto;
             }
-          
-            return presupuesto;
-          }
-
-
-
+            
             let consultar = document.getElementById("consultar");
             consultar.addEventListener("submit", presupuesto);
-            let presupuestoEncontrado = presupuestoForm();
             
-            function presupuesto() {
-                e.preventDefault();
+            function presupuesto(e) {
+              e.preventDefault();
               let consultarPresupuesto = document.getElementById("resultadoPresupuesto");
+              let presupuestoEncontrado = presupuestoForm(e);
+            
               if (presupuestoEncontrado) {
-                mensaje.innerHTML = ("El precio final es de: $" + presupuestoEncontrado);
+                consultarPresupuesto.innerHTML = "El precio final es de: $" + presupuestoEncontrado;
               } else {
-                mensaje.innerHTML = ("El precio final es de: $" + presupuestoEncontrado);
+                consultarPresupuesto.innerHTML = "No se encontró presupuesto.";
               }
             }
+            
 
   
         //Storage & JSON (reparado codigo)
 
-function guardarLocalStorage(e){
-e.preventDefault();
-let persona = {
-nombre:document.getElementById("first-name").value,
-apellido:document.getElementById("last-name").value,
-email:document.getElementById("email").value,
-edad:document.getElementById("age").value
-};
-localStorage.setItem('persona', JSON.stringify(persona));
+function guardarLocalStorage(e) {
+  e.preventDefault();
+  
+  let persona = {
+    nombre: document.getElementById("first-name").value,
+    apellido: document.getElementById("last-name").value,
+    email: document.getElementById("email").value,
+    edad: document.getElementById("age").value
+  };
+  
+  localStorage.setItem('persona', JSON.stringify(persona));
 }
 
-function obtenerLocalStorage(){
-if(localStorage.getItem('persona')){
-let persona = JSON.parse(localStorage.getItem('persona'));
-console.log(persona);
-} else {console.log("No hay registros")};
+function obtenerLocalStorage() {
+  if (localStorage.getItem('persona')) {
+    let persona = JSON.parse(localStorage.getItem('persona'));
+    console.log(persona);
+  } else {
+    console.log("No hay registros");
+  }
 }
 
-
-let send = document.getElementById("send");
-send.addEventListener("submit", (e)=> { 
-    guardarLocalStorage (e); 
-    obtenerLocalStorage ();
-});       
+document.getElementById("send").addEventListener("submit", (e) => {
+  guardarLocalStorage(e);
+  obtenerLocalStorage();
+});
