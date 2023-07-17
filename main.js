b462fe9ec83f410a0d21cd654f70d86458a84483
Magaly Gallardo <maggye.chayns@gmail.com>
2023-07-16 23:44:05 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,121 @@
+            // Consultar precio de actividad (hay que escribir con las respectivas mayúsculas)
+  
+            let actividadBuscada = document.getElementById("busquedaActividad");
+
+            function busquedaActividad() {
+              const precioActividad = [
+                { nombre: "Pase Libre", precio: 5000 },
+                { nombre: "CrossFit", precio: 3000 },
+                { nombre: "GAP", precio: 2500 },
+                { nombre: "Zumba", precio: 4200 },
+                { nombre: "Step", precio: 3500 },
+                { nombre: "Musculación", precio: 4000 },
+                { nombre: "Spinning", precio: 4100 },
+              ];
+            
+              return precioActividad.find((el) => el.nombre === actividadBuscada.value);
+            }
+
+            let buscar = document.getElementById("buscar");
+            buscar.addEventListener("click", respuestaClick);
+
+            
+            function respuestaClick() {
+              let mensaje = document.getElementById("resultadoActividadBuscada");
+              let actividadEncontrada = busquedaActividad();
+              
+              if (actividadEncontrada) {
+                mensaje.innerHTML = ("El precio de la actividad es de: $" + actividadEncontrada.precio);
+              } else {
+                mensaje.innerHTML = "Actividad no encontrada";
+              }
+            }
+            
+            
+            //Presupuesto
+         
+           
+           
+            /*
+            
+
+               function presupuestoForm() {
+              e.preventDefault();
+            let presupuesto = 0;
+            if (radioUnoSeleccionado.value === "si" && radioTresSeleccionado.name === "masculino") {
+              presupuesto = (actividadPresupuesto / 2) - (actividadPresupuesto * 10 / 100);
+            } else if (radioUnoSeleccionado.value === "si" && radioCuatroSeleccionado.name === "femenino") {
+              presupuesto = (actividadPresupuesto / 2) - (actividadPresupuesto * 5 / 100);
+            } else if (radioDosSeleccionado.value === "no" && radioSeleccionado.name === "masculino") {
+              presupuesto = (actividadPresupuesto * 10) / 100;
+            } else {
+              presupuesto = (actividadPresupuesto * 5) / 100;
+            }
+          
+            return presupuesto;
+          };
+
+           
+          let actividadPresupuesto = document.querySelector("#actividadPresupuesto");
+          const radioSeleccionado = document.querySelector('input[type="radio"]:checked');
+          function presupuestoForm() {
+              e.preventDefault();
+            let presupuesto = 0;
+          
+            if (radioSeleccionado.value === "si" && radioSeleccionado.name === "masculino") {
+              presupuesto = (actividadPresupuesto.value / 2) - (actividadPresupuesto.value * 10 / 100);
+            } else if (radioSeleccionado.value === "si" && radioSeleccionado.name === "femenino") {
+              presupuesto = (actividadPresupuesto.value / 2) - (actividadPresupuesto.value * 5 / 100);
+            } else if (radioSeleccionado.value === "no" && radioSeleccionado.name === "masculino") {
+              presupuesto = (actividadPresupuesto.value * 10) / 100;
+            } else {
+              presupuesto = (actividadPresupuesto.value * 5) / 100;
+            }
+          
+            return presupuesto;
+          }
+
+
+
+            let consultar = document.getElementById("consultar");
+            consultar.addEventListener("click", presupuesto);
+            let presupuestoEncontrado = presupuestoForm();
+            
+            function presupuesto() {
+                e.preventDefault();
+              let consultarPresupuesto = document.getElementById("resultadoPresupuesto");
+             
+            
+              if (presupuestoEncontrado) {
+                mensaje.innerHTML = ("El es de: $" + presupuestoEncontrado);
+              } else {
+                mensaje.innerHTML = ("El es de: $" + presupuestoEncontrado);
+              }
+            }
+ */
+        //Storage & JSON (reparado codigo)
+
+function guardarLocalStorage(e){
+e.preventDefault();
+let persona = {
+nombre:document.getElementById("first-name").value,
+apellido:document.getElementById("last-name").value,
+email:document.getElementById("email").value,
+edad:document.getElementById("age").value
+};
+localStorage.setItem('persona', JSON.stringify(persona));
+}
+
+function obtenerLocalStorage(){
+if(localStorage.getItem('persona')){
+let persona = JSON.parse(localStorage.getItem('persona'));
+console.log(persona);
+} else {console.log("No hay registros")};
+}
+
+
+let send = document.getElementById("send");
+send.addEventListener("submit", (e)=> { 
+    guardarLocalStorage (e); 
+    obtenerLocalStorage ();
+});       
