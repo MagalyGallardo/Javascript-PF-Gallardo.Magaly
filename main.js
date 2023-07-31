2d354f19540021e1965b0181669575b436fae9ed
Magaly Gallardo <maggye.chayns@gmail.com>
2023-07-31 20:21:23 -0300

    Se arregla API, storage y Operador Ternario

--- main.js
+++ main.js
@@ -1,164 +1,149 @@
 document.addEventListener('DOMContentLoaded', function () {
   Swal.fire({
-    icon: 'info',
-    title: '¡Bienvenido!',
-    text: 'Si es tu primera vez, tenés un 20% de descuento en tu presupuesto.',
+  icon: 'info',
+  title: '¡Bienvenido!',
+  text: 'Si es tu primera vez, tenés un 20% de descuento en tu presupuesto.',
+  confirmButtonText: 'Aceptar'
+  });
+  });
+
+// Consultar precio de actividad (hay que escribir con las respectivas mayúsculas) 
+
+let actividadBuscada = document.getElementById("busquedaActividad");
+
+function busquedaActividad() {
+    const precioActividad = [
+    { nombre: "Pase Libre", precio: 5000 },
+    { nombre: "CrossFit", precio: 3000 },
+    { nombre: "GAP", precio: 2500 },
+    { nombre: "Zumba", precio: 4200 },
+    { nombre: "Step", precio: 3500 },
+    { nombre: "Musculación", precio: 4000 },
+    { nombre: "Spinning", precio: 4100 },
+  ];
+  return precioActividad.find((el) => el.nombre === actividadBuscada.value);
+}
+
+let buscar = document.getElementById("buscar");
+buscar.addEventListener("click", respuestaClick);
+
+
+function respuestaClick() {
+  let mensaje = document.getElementById("resultadoActividadBuscada");
+  let actividadEncontrada = busquedaActividad();
+
+  if (actividadEncontrada) {
+  mensaje.innerHTML = ("El precio de la actividad es de: $" + actividadEncontrada.precio);
+  } else {
+  mensaje.innerHTML = "Actividad no encontrada";
+  }
+}
+
+
+//Presupuesto
+
+function calcularPresupuesto() {
+
+  const actividad = document.getElementById('actividadPresupuesto').value;
+  const isFirstTime = document.querySelector('input[name="primero"]:checked').value === 'si';
+  const genero = document.querySelector('input[name="genero"]:checked').value;
+
+
+  let presupuesto = parseFloat(actividad);
+  isFirstTime ? (presupuesto *= 0.8) : null;
+  genero === 'femenino' ? (presupuesto *= 0.9) : null;
+
+  document.getElementById('resultadoPresupuesto').textContent = `Tu presupuesto es de: $${presupuesto.toFixed(2)}`;
+}
+
+document.getElementById('consultar').addEventListener('submit', function (event) {
+  event.preventDefault(); 
+  calcularPresupuesto(); 
+});
+
+document.getElementById('primeraVez').addEventListener('click', function () {
+  Swal.fire({
+  icon: 'success',
+  title: 'Descuento por Primera Vez',
+  text: '¡Tenés un 20% de descuento en tu presupuesto!',
+  confirmButtonText: 'Aceptar'
+  });
+});
+document.getElementById('generoFemenino').addEventListener('click', function () {
+  Swal.fire({
+  icon: 'success',
+  title: 'Descuento por Género Femenino',
+  text: '¡Tenés un 10% de descuento en tu presupuesto!',
+  confirmButtonText: 'Aceptar'
+  });
+});
+
+//Storage & JSON 
+function guardarLocalStorage(e) {
+  e.preventDefault();
+
+  const persona = {
+    nombre: document.getElementById("first-name").value,
+    apellido: document.getElementById("last-name").value,
+    email: document.getElementById("email").value,
+    edad: document.getElementById("age").value
+  };
+
+  let personas = obtenerPersonasRegistradas();
+  personas.push(persona);
+  localStorage.setItem('personas', JSON.stringify(personas));
+  console.log(personas); // Opcional: Mostrar el array de personas en la consola para verificar
+}
+
+function obtenerPersonasRegistradas() {
+  let personas = [];
+  if (localStorage.getItem('personas')) {
+    personas = JSON.parse(localStorage.getItem('personas'));
+  }
+  return personas;
+}
+
+document.getElementById("send").addEventListener("submit", (e) => {
+  guardarLocalStorage(e);
+  Swal.fire({
+    icon: 'success',
+    title: 'Te has registrado exitosamente',
+    text: '¡Gracias por registrarte en Spartan Fitness Center!',
     confirmButtonText: 'Aceptar'
   });
 });
 
-// Consultar precio de actividad (hay que escribir con las respectivas mayúsculas) 
-            
-          let actividadBuscada = document.getElementById("busquedaActividad");
+//Modal de términos y condiciones
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
 
-          function busquedaActividad() {
-            const precioActividad = [
-              { nombre: "Pase Libre", precio: 5000 },
-              { nombre: "CrossFit", precio: 3000 },
-              { nombre: "GAP", precio: 2500 },
-              { nombre: "Zumba", precio: 4200 },
-              { nombre: "Step", precio: 3500 },
-              { nombre: "Musculación", precio: 4000 },
-              { nombre: "Spinning", precio: 4100 },
-            ];
-            return precioActividad.find((el) => el.nombre === actividadBuscada.value);
-          }
+myModal.addEventListener('shown.bs.modal', () => {
+myInput.focus()
+})
 
-          let buscar = document.getElementById("buscar");
-          buscar.addEventListener("click", respuestaClick);
+//API Indice de Masa Corporal
+const urlApi = 'https://body-mass-index-bmi-calculator.p.rapidapi.com/metric';
 
-                      
-          function respuestaClick() {
-            let mensaje = document.getElementById("resultadoActividadBuscada");
-            let actividadEncontrada = busquedaActividad();
-                        
-            if (actividadEncontrada) {
-              mensaje.innerHTML = ("El precio de la actividad es de: $" + actividadEncontrada.precio);
-              } else {
-              mensaje.innerHTML = "Actividad no encontrada";
-              }
-            }
-                      
-                      
-            //Presupuesto
+async function obtenerIMC(e) {
+  e.preventDefault();
+  const altura = document.getElementById("altura").value;
+  const peso = document.getElementById("peso").value;
 
-              function calcularPresupuesto() {
-                
-                const actividad = document.getElementById('actividadPresupuesto').value;
-                const isFirstTime = document.querySelector('input[name="primero"]:checked').value === 'si';
-                const genero = document.querySelector('input[name="genero"]:checked').value;
+  try {
+    const response = await fetch(`${urlApi}?weight=${peso}&height=${altura}`, {
+    method: 'GET',
+    headers: {
+    'X-RapidAPI-Key': '93994dc17cmshb906990761073cdp1b7dc3jsn57fb06bb9b81',
+    'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
+    },
+});
 
-              
-                let presupuesto = parseFloat(actividad);
-                if (isFirstTime) {
-                  presupuesto *= 0.8; 
-                }
-                if (genero === 'femenino') {
-                  presupuesto *= 0.9; 
-                }
-                
-                document.getElementById('resultadoPresupuesto').textContent = `Tu presupuesto es de: $${presupuesto.toFixed(2)}`;
-              }
-
-              document.getElementById('consultar').addEventListener('submit', function (event) {
-                event.preventDefault(); 
-                calcularPresupuesto(); 
-              });
-              
-              document.getElementById('primeraVez').addEventListener('click', function () {
-                Swal.fire({
-                  icon: 'success',
-                  title: 'Descuento por Primera Vez',
-                  text: '¡Tenés un 20% de descuento en tu presupuesto!',
-                  confirmButtonText: 'Aceptar'
-                });
-              });
-              document.getElementById('generoFemenino').addEventListener('click', function () {
-                Swal.fire({
-                  icon: 'success',
-                  title: 'Descuento por Género Femenino',
-                  text: '¡Tenés un 10% de descuento en tu presupuesto!',
-                  confirmButtonText: 'Aceptar'
-                });
-              });
-
-        //Storage & JSON 
-
-            function guardarLocalStorage(e) {
-              e.preventDefault();
-              
-              let persona = {
-                nombre: document.getElementById("first-name").value,
-                apellido: document.getElementById("last-name").value,
-                email: document.getElementById("email").value,
-                edad: document.getElementById("age").value
-              };
-              
-              let personas = obtenerPersonasRegistradas();
-              personas.push(persona);
-              localStorage.setItem('persona', JSON.stringify(persona));
-            }
-
-            function obtenerPersonasRegistradas() {
-              let personas = [];
-              if (localStorage.getItem('personas')) {
-                personas = JSON.parse(localStorage.getItem('personas'));
-              }
-              return personas;
-            }
-
-            function obtenerLocalStorage() {
-              if (localStorage.getItem('persona')) {
-                let persona = JSON.parse(localStorage.getItem('persona'));
-                console.log(persona);
-              } else {
-                console.log("No hay registros");
-              }
-            }
-
-            document.getElementById("send").addEventListener("submit", (e) => {
-              guardarLocalStorage(e);
-              obtenerLocalStorage();
-              Swal.fire({
-                icon: 'success',
-                title: 'Te has registrado exitosamente',
-                text: '¡Gracias por registrarte en Spartan Fitness Center!',
-                confirmButtonText: 'Aceptar'
-              });
-            });
-
-            //Modal de términos y condiciones
-            const myModal = document.getElementById('myModal')
-            const myInput = document.getElementById('myInput')
-
-            myModal.addEventListener('shown.bs.modal', () => {
-            myInput.focus()
-          })
-
-          //API Indice de Masa Corporal
-          const urlApi = 'https://body-mass-index-bmi-calculator.p.rapidapi.com/metric';
-
-          async function obtenerIMC() {
-            const altura = document.getElementById("altura").value;
-            const peso = document.getElementById("peso").value;
-     
-            try {
-              const response = await fetch(`${urlApi}?weight=${peso}&height=${altura}`, {
-                method: 'GET',
-                headers: {
-                  'X-RapidAPI-Key': '93994dc17cmshb906990761073cdp1b7dc3jsn57fb06bb9b81',
-                  'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
-                },
-              });
-          
-              const data = await response.json();
-              const resultadoIMC = data.bmi.toFixed(2); 
-              document.getElementById("resultadoImc").innerHTML = `Tu IMC es: ${resultadoIMC}`;
-            } catch (error) {
-              console.error(error);
-            }
-            document.getElementById("calcular").addEventListener("submit", obtenerIMC);
-            e.preventDefault();
-          }
-
-        
+const data = await response.json();
+console.log(data);
+const resultadoIMC = data.bmi.toFixed(2); 
+document.getElementById("resultadoImc").innerHTML = `Tu IMC es: ${resultadoIMC}`; //el resultado da siempre 0.00 porque es un numero muy largo que he intentado cambiar pero es la configuracion de la API (tiene que elevar al cuadrado la altura pero no lo hace). También intenté con otra y me pasaba lo mismo.
+  } catch (error) {
+  console.error(error);
+  }
+}
+document.getElementById("calcular").addEventListener("submit", obtenerIMC);
